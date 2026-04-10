a3e26bb76478ecc14fcf300462c0135263d170b5
Christopher <christofaokoro@gmail.com>
2026-04-10 23:10:24 +0100

    refactor: restructure ckb-client to improve client creation and connection checking

--- lib/ckb-client.ts
+++ lib/ckb-client.ts
@@ -1,13 +1,30 @@
-import { ccc } from '@ckb-ccc/core'
+import { ccc } from "@ckb-ccc/core";
 
-// Change to 'testnet' when ready for public network
-const NETWORK = 'devnet'
+export type Network = "devnet" | "testnet";
 
-export const ckbClient = NETWORK === 'testnet'
-  ? new ccc.ClientPublicTestnet()
-  : new ccc.ClientPublicTestnet({
-      url: 'http://localhost:28114',
-    })
+// Change to "testnet" when you want to use the public CKB testnet.
+const NETWORK: Network = "devnet";
+
+export function createCkbClient(network: Network = NETWORK) {
+  return network === "testnet"
+    ? new ccc.ClientPublicTestnet()
+    : new ccc.ClientPublicTestnet({
+        url: "http://localhost:28114",
+      });
+}
+
+export const ckbClient = createCkbClient();
+
+export async function checkCkbConnection(network: Network = NETWORK) {
+  const client = createCkbClient(network);
+  const tip = await client.getTip();
+
+  return {
+    network,
+    url: client.url,
+    tip: tip.toString(),
+  };
+}
 
 export const PRIVATE_KEY =
-  '0x6109170b275a09ad54877b82f7d9930f88cab5717d484fb4741ae9d1dd078cd6'
+  "0x6109170b275a09ad54877b82f7d9930f88cab5717d484fb4741ae9d1dd078cd6";
