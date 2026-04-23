40a801e96d33a067ced8b7e1dfcb15d9c5e83c54
Christopher <christofaokoro@gmail.com>
2026-04-23 04:35:54 +0100

    Remove PRIVATE_KEY declaration and getPrivateKey function from ckb-client.ts

--- lib/ckb-client.ts
+++ lib/ckb-client.ts
@@ -9,13 +9,4 @@ export const ckbClient = new ccc.ClientPublicTestnet({
   url: CKB_RPC_URL,
 });
 
-// Test account private key 
-export const PRIVATE_KEY = process.env.CKB_PRIVATE_KEY;
 
-
- export const getPrivateKey = () => {
-  if (!PRIVATE_KEY) {
-    throw new Error("SECURE_ERROR: CKB_PRIVATE_KEY is not defined in environment variables.");
-  }
-  return PRIVATE_KEY;
-};
